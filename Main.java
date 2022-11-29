53cb519b05976daff1adb59cbba202e2ba772165
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2022-11-29 13:55:48 +0000

    playing with java for the first time

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main{
+    // public static void main(String args[]){
+    //     System.out.println("Hello, World!"); 
+    // }
+    public static void main(String args[]){
+        String message = getString();
+        System.out.println(message); 
+    }
+    public static String getString(){
+        return "Hello from return";
+    }
+
+}
