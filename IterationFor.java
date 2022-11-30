4fea0734e5dbeb6737fc332fccd5295b99dd1203
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2022-11-30 11:33:10 +0000

    Iteration For Loop

---
+++ IterationFor.java
@@ -0,0 +1,7 @@
+public class IterationFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello from For Loop");
+        }
+    }
+}
