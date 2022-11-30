844b48e2757af92d2230a45d0b7c06431f9e2b36
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2022-11-30 11:22:02 +0000

    Iteration Do While Loop

---
+++ IterationDoWhile.java
@@ -0,0 +1,13 @@
+public class IterationDoWhile {
+    public static void main(String[] args) {
+        int j = 5;
+        do {
+            System.out.println("Hello from Do While");
+            j++;
+        } while (j <= 7);
+
+        do {
+            System.out.println("It will print at least once");
+        } while (j<=3);
+    }
+}
