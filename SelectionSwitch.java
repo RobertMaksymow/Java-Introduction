e9e1013053e9a3d0f54d0266420cf8417274eaad
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2022-11-30 10:42:52 +0000

    Switch Statement

--- SelectionSwitch.java
+++ SelectionSwitch.java
@@ -1,3 +1,32 @@
 public class SelectionSwitch {
+    public static void main(String[] args) {
+        int n = 2;
     
+        //If Statement
+        if(n == 1){
+            System.out.println("One");
+        }
+        else if(n == 2){
+            System.out.println("Two");
+        }
+        else{
+            System.out.println("Four");
+        }
+
+        // Switch Statement (supports Characters and Strings, don't supports doubles)
+        switch (n) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("No number matched");
+                break;
+        }
+    }
 }
