2ee87cad930b0f3b9750bdf136a47af404805c3d
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2022-12-01 16:19:19 +0000

    Numbers formatting basics - £ %

---
+++ NumbersFormatting.java
@@ -0,0 +1,17 @@
+import java.text.NumberFormat;
+
+public class NumbersFormatting {
+ public static void main(String[] args) {
+   NumberFormat currency = NumberFormat.getCurrencyInstance();  //Abstract class  has  .getCurrencyInstance 'factory' method / halfbaked cake. Creates new objects.
+   String result = currency.format(1234567.891);
+   System.out.println(result); // £1,234,567.89
+
+  NumberFormat percent = NumberFormat.getPercentInstance();
+  String result2 = percent.format(0.1);
+  System.out.println(result2);  // 10%
+
+  //Method chaining
+  String result3 = NumberFormat.getPercentInstance().format(0.05);
+  System.out.println(result3);
+ }   
+}
